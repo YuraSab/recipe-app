2ccd599352322e851890d8b27c649110618ea450
YuraSab <yrasabat@gmail.com>
2024-12-11 11:26:35 +0200

    patch "search by debounce"

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useLayoutEffect, useState} from 'react';
 import styles from "./SearchBar.module.css";
 import {UpdateURLFunction} from "../../pages/Recipes/Recipes";
+import {useLocation} from "react-router-dom";
 
 type SearchBarProps = {
     handleSearch: (query: string) => void,
@@ -8,24 +9,46 @@ type SearchBarProps = {
 }
 const SearchBar:React.FC<SearchBarProps> = ({ handleSearch, updateURL }) => {
 
-    const [query, setQuery] = useState<string>("");
+    const location = useLocation();
 
-    const updateSearchQuery = () => {
-        updateURL({ search: query, page: "1" });
-        handleSearch(query);
-    }
+    const [query, setQuery] = useState<string>("");
+    const [previousQuery, setPreviousQuery] = useState<string>("");
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            console.log("query", query);
+            if (query !== previousQuery) {
+                setPreviousQuery(query);
+                updateURL({ search: query, page: "1" });
+                handleSearch(query);
+            }
+        }, 1500);
+        return () => clearTimeout(timer);
+    }, [query, updateURL, handleSearch]);
+
+
+    useLayoutEffect(() => {
+        const params = new URLSearchParams(location.search);
+        const searchParams = params.get("search") || "";
+        setPreviousQuery(searchParams);
+        setQuery(searchParams);
+    }, []);
+
+    // const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setQuery(event.target.value);
+    // }
 
     return (
         <div className={styles.searchContainer}>
-            <input
-                type={"text"}
-                placeholder={"Пошук рецепту"}
-                value={query}
-                onChange={(event) => setQuery(event.target.value)}
-            />
-            <button onClick={updateSearchQuery}>&#x260C;</button>
+                <input
+                    type={"text"}
+                    placeholder={"Пошук рецепту"}
+                    value={query}
+                    onChange={(event) => setQuery(event.target.value)}
+                    // onChange={handleInputChange}
+                />
         </div>
-    );
+);
 };
 
 export default SearchBar;
